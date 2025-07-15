f658d82c23fee378a79f7afa9ba5f7aa4d7ce25a
Alex Sandro <alexsandro49.dev@gmail.com>
2025-07-14 23:25:54 -0300

    Updates

---
+++ assets/AccountView-kMeiluTN.js
@@ -0,0 +1,1 @@
+import{y as e,e as c,o as n}from"./index-BAaQZIrD.js";const o={};function t(r,a){return n(),c("h1",null,"Account page")}const _=e(o,[["render",t]]);export{_ as default};
---
+++ assets/OrdersView-9iXWOCom.js
@@ -0,0 +1,1 @@
+import{y as e,e as r,o as c}from"./index-BAaQZIrD.js";const n={};function o(s,t){return c(),r("h1",null,"Orders page")}const _=e(n,[["render",o]]);export{_ as default};
